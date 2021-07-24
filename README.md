501df041b1a10bce342bd4398ead4486d5a31c3d
yasindu <yasindusathsara@gmail.com>
2021-07-24 11:58:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a nowty app.....
+
+npm install
+ng serve --open
+
+for deploy - ng build --baseurl=''
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) 2018 - present IJSE. All rights reserved.
+ *  *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  *--------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @author Yasindu Sathsara <yasindu@ijse.lk> (ijse.lk)
+ * @since 7/24/21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello X-App");
+    }
+}
